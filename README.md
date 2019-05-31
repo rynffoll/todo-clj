4d25f6806d06eeab83b4eeda0d2790fec75c4c79
Ruslan Kamashev <rynffoll@gmail.com>
2019-05-31 23:50:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# todo-clj
+
+## Usage
+
+Run the project directly:
+
+    $ clj -m todo-clj.core
+
+Run the project's tests (they'll fail until you edit them):
+
+    $ clj -A:test:runner
+
---
+++ test/core_test.clj
@@ -0,0 +1,7 @@
+(ns rynffoll.todo-clj-test
+  (:require [clojure.test :refer :all]
+            [rynffoll.todo-clj :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
