acbf1ea30d99b4dad30feaea1262e3543fbdbf19
Ruslan Kamashev <rynffoll@gmail.com>
2019-07-10 21:38:53 +0300

    Add integrant-repl

---
+++ env/dev/user.clj
@@ -0,0 +1,31 @@
+(ns user ;; dev.user
+  (:require [todo_clj.system :as sys]
+            [clojure.tools.deps.alpha.repl :refer [add-lib]]
+            [integrant.repl :refer [clear go halt prep init reset reset-all]]))
+
+(integrant.repl/set-prep! (fn [] sys/config))
+
+(comment
+
+  ;; http://insideclojure.org/2018/05/04/add-lib/
+  (require '[clojure.tools.deps.alpha.repl :refer [add-lib]])
+
+  (add-lib 'integrant/repl {:mvn/version "0.3.1"})
+
+  (require '[integrant.repl :refer [clear go halt prep init reset reset-all]])
+
+  (integrant.repl/set-prep! (fn [] sys/config))
+
+  (prep)
+
+  (init)
+
+  integrant.repl.state/system
+
+  (go)
+
+  (halt)
+
+  (reset)
+
+  )
